da4a917f2cf54610c4464af5ee022dc82adb3402
monikossite <monikossite@gmail.com>
2018-03-11 19:50:59 +0200

    task finished

--- src/lt/swedbank/interestcalculator/CompoundInterestCalculator.java
+++ src/lt/swedbank/interestcalculator/CompoundInterestCalculator.java
@@ -5,79 +5,61 @@ import java.util.Scanner;
 
 public class CompoundInterestCalculator {
 
-    //I would not suggest using global variables here.
-    //Better solution would be to create separate methods for every input: ex.: getAmount(), getInterestRate(), etc.
-
-    //M: I agree. But on monday we "didn't know" these methods so I was trying to get the result without getters and setters.
-    //Should I change now?
     static int amount;
-    static int interestRate;
+    static int interestRate[] = new int[1];
     static int periodLength;
     static String compoundFrequency;
-
-    //Application is not behaving as expected.
-    //If "periodLength" = 3 and "compoundFrequency" = "M", it is expected that program would print:
-    //  3 "Interest amount after year ..." lines
-    //  +
-    //  "InterestAmounts" array containing 36 elements (3*12)
-    //
-    // Since in task's description this is not clearly stated, I'll let it slide this time :)
-
-    //M: I tried to fix it. Is it behaving as should be right now?
+    static int i = 0;
 
     public static void main(String[] args) {
-        //Do not declare your local variables separately from their initialization.
-        //Variables should be declared where they're first used/needed.
-        //M: Mindaugas told me completely opposite, I don't understand.
+
         int compoundFrequencyNumber;
-        int arrayLength ;
+        int arrayLength;
         int index;
+        int interestRateIndex;
         double compoundingFrequency = 0;
-        double[] interestAmountAfterYear;
-        double[] interestAmounts;
-        double[] interestAmountAfterIteration;
-        String arrayString;
+        double[][] interestAmounts;
+        double[][] interestAmountAfterIteration;
 
         readInput();
 
         compoundFrequencyNumber = findFrequencyNumber(compoundFrequency);
 
         arrayLength = compoundFrequencyNumber * periodLength;
-        interestAmounts = new double[arrayLength];
-        interestAmountAfterYear = new double[periodLength + 1];
-        interestAmountAfterIteration = new double[arrayLength];
+        interestAmounts = new double[arrayLength][interestRate.length];
+        interestAmountAfterIteration = new double[arrayLength][interestRate.length];
 
-        for (index = 0 ; index < arrayLength; index++) {
-            compoundingFrequency = calculateCompoundingFrequency(compoundFrequencyNumber, index + 1);
-            interestAmountAfterIteration[index] = compoundingFrequency - amount;
-
-            if (index == 0) {
-                interestAmounts[0] = interestAmountAfterIteration[index];
-            }
-            else {
-                interestAmounts[index] = interestAmountAfterIteration[index] - interestAmountAfterIteration[index - 1];
-            }
-
+        if (interestRate[interestRate.length - 1] == 0) {
+            interestRate = Arrays.copyOf(interestRate, interestRate.length - 1);
+            System.out.println(interestRate.length + " " + interestRate[interestRate.length - 1]);
         }
 
-        for (index = 1; index <= periodLength; index++ ) {
-            compoundingFrequency = calculateCompoundingFrequency(compoundFrequencyNumber, index);
-            interestAmountAfterYear[index] = compoundingFrequency - amount;
+        for (interestRateIndex = 0; interestRateIndex < interestRate.length; interestRateIndex++) {
+            for (index = 0; index < arrayLength; index++) {
+                compoundingFrequency =
+                        calculateCompoundingFrequency(compoundFrequencyNumber, index + 1, interestRateIndex);
 
-            //M: Does this print correctly?
-            System.out.print("Interest amount after year " + (index));
-            System.out.printf(": %.2f\n", interestAmountAfterYear[index]);
+                interestAmountAfterIteration[interestRateIndex][index] = compoundingFrequency - amount;
+
+                if (index == 0) {
+                    interestAmounts[interestRateIndex][0] = interestAmountAfterIteration[interestRateIndex][index];
+
+                } else {
+                    interestAmounts[interestRateIndex][index] =
+                            interestAmountAfterIteration[interestRateIndex][index] - interestAmountAfterIteration[interestRateIndex][index - 1];
+                }
+
+                System.out.printf(" %.2f", interestAmounts[interestRateIndex][index]);
+            }
+
+            System.out.println();
+
         }
-
-        arrayString = Arrays.toString(interestAmounts);
-
-        System.out.println(arrayString);
-        System.out.printf("Total amount: %.2f\n", compoundingFrequency);
 
     }
 
-    private static double calculateCompoundingFrequency(int compoundFrequencyNumber, int year) {
-        double powerBase = 1 + (interestRate * 0.01) / compoundFrequencyNumber;
+    private static double calculateCompoundingFrequency(int compoundFrequencyNumber, int year, int interestRateIndex) {
+        double powerBase = 1 + (interestRate[interestRateIndex] * 0.01) / compoundFrequencyNumber;
         double powerExponent = year * compoundFrequencyNumber;
 
         return amount * Math.pow(powerBase, powerExponent);
@@ -89,9 +71,14 @@ public class CompoundInterestCalculator {
         System.out.print("Amount: ");
         amount = scanner.nextInt();
 
-        System.out.print("Interest rate (%): ");
-        interestRate = scanner.nextInt();
+        do {
+            System.out.print("Interest rate (%): ");
+            interestRate = Arrays.copyOf(interestRate, i + 1);
+            interestRate[i] = scanner.nextInt();
+            i++;
 
+        }
+        while (!(interestRate[i - 1] == 0));
         System.out.print("Period length(years): ");
         periodLength = scanner.nextInt();
 
@@ -112,10 +99,8 @@ public class CompoundInterestCalculator {
                 return 4;
             case "H":
                 return 2;
-            //"case "Y":" can be removed, since "default:" is already returning "1".
-            //M: Now I switched it to default if someone would like to run the code with 'Y' but don't know that 1 year is default. Or is it also wrong?
             default:
-                case "Y":
+            case "Y":
                 return 1;
         }
     }
